de245cd6588f45ae66b02f3af8a6c8cf452357f9
Griffin Heyrich <76177337+griffinandrew@users.noreply.github.com>
2023-06-20 16:35:31 -0400

    add testing infastructure

--- tests/mkscreenshots.py
+++ tests/mkscreenshots.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import platform
 import sys
+import time
 
 if len(sys.argv) <= 1:
     print("Invalid number of arguments")
@@ -21,23 +22,30 @@ if platform.system() == 'Windows' or platform.system() == 'Darwin':
     driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager().install()), options=chrome_options)
 elif platform.system() == 'Linux':
     chrome_options.add_argument('--headless') # Inorder for Action to run, it needs to be headless
-    driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=chrome_options)
+    driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager().install(), chrome_type=ChromeType.GOOGLE), options=chrome_options)
+    #add this from rise_test bc complaining about version of chrome in this test now
+    #driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=chrome_options)
 else:
     print("Unknown OS")
     exit(1)
 
 driver.get(url)
 
-driver.implicitly_wait(10)
+time.sleep(10)
+
+driver.save_screenshot('main_screenshot.png')
+print("Saved main screenshot")
 
 new_nb_button = driver.find_element(By.XPATH, '//div[@data-category="Notebook"]')
 new_nb_button.click()
+time.sleep(10)
 
+driver.save_screenshot('new_nb_screenshot.png')
+print("Saved new notebook screenshot")
 
 rise_button = driver.find_element(By.XPATH, '//button[@data-command="RISE:preview"]')
 rise_button.click()
+time.sleep(10)
 
-print("Rise Extension is working!")
-
-
-
+driver.save_screenshot('rise_screenshot.png')
+print("Saved rise screenshot")
