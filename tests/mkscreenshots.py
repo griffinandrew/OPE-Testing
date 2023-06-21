af1c9ad18d460eab20e9b89cd67abb3b46377c4c
Griffin Heyrich <76177337+griffinandrew@users.noreply.github.com>
2023-06-21 12:56:19 -0400

    add testing infastructure

--- tests/mkscreenshots.py
+++ tests/mkscreenshots.py
@@ -22,7 +22,9 @@ if platform.system() == 'Windows' or platform.system() == 'Darwin':
     driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager().install()), options=chrome_options)
 elif platform.system() == 'Linux':
     chrome_options.add_argument('--headless') # Inorder for Action to run, it needs to be headless
-    driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=chrome_options)
+    driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager().install(), chrome_type=ChromeType.GOOGLE), options=chrome_options)
+    #add this from rise_test bc complaining about version of chrome in this test now
+    #driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=chrome_options)
 else:
     print("Unknown OS")
     exit(1)
@@ -31,7 +33,20 @@ driver.get(url)
 
 time.sleep(10)
 
-driver.save_screenshot('screenshot.png')
+driver.save_screenshot('main_screenshot.png')
+print("Saved main screenshot")
 
+new_nb_button = driver.find_element(By.XPATH, '//div[@data-category="Notebook"]')
+new_nb_button.click()
+time.sleep(10)
 
+driver.save_screenshot('new_nb_screenshot.png')
+print("Saved new notebook screenshot")
+
+rise_button = driver.find_element(By.XPATH, '//button[@data-command="RISE:preview"]')
+rise_button.click()
+time.sleep(10)
+
+driver.save_screenshot('rise_screenshot.png')
+print("Saved rise screenshot")
 
