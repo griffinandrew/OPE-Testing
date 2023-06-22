187137f5eeb1ec702fcfd400700098f0fbae2c8b
Griffin Heyrich <76177337+griffinandrew@users.noreply.github.com>
2023-06-22 11:00:50 -0400

    add testing infastructure

--- tests/mkscreenshots.py
+++ tests/mkscreenshots.py
@@ -6,6 +6,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import platform
 import sys
+import time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 if len(sys.argv) <= 1:
     print("Invalid number of arguments")
@@ -21,23 +24,35 @@ if platform.system() == 'Windows' or platform.system() == 'Darwin':
     driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager().install()), options=chrome_options)
 elif platform.system() == 'Linux':
     chrome_options.add_argument('--headless') # Inorder for Action to run, it needs to be headless
-    driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=chrome_options)
+    driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager().install(), chrome_type=ChromeType.GOOGLE), options=chrome_options)
+    #add this from rise_test bc complaining about version of chrome in this test now
+    #driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=chrome_options) #kes og command
 else:
     print("Unknown OS")
     exit(1)
 
 driver.get(url)
 
-driver.implicitly_wait(10)
+time.sleep(10)
 
-new_nb_button = driver.find_element(By.XPATH, '//div[@data-category="Notebook"]')
+driver.save_screenshot('main_screenshot.png')
+print("Saved main screenshot")
+
+wait = WebDriverWait(driver, 10)  # Wait for a maximum of 10 seconds
+
+new_nb_button = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@data-category="Notebook"]')))
+#new_nb_button = driver.find_element(By.XPATH, '//div[@data-category="Notebook"]')
+
 new_nb_button.click()
+time.sleep(10)
 
+driver.save_screenshot('new_nb_screenshot.png')
+print("Saved new notebook screenshot")
 
 rise_button = driver.find_element(By.XPATH, '//button[@data-command="RISE:preview"]')
 rise_button.click()
+time.sleep(10)
 
-print("Rise Extension is working!")
-
-
+driver.save_screenshot('rise_screenshot.png')
+print("Saved rise screenshot")
 
